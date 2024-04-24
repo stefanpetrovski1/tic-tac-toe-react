4d7852e662f192186d6c18086d9ed27e98ba372e
Stefan Petrovski <spetrovski09@gmail.com>
2024-04-25 01:08:56 +0200

    Add field styles

--- src/Field.tsx
+++ src/Field.tsx
@@ -1,9 +1,17 @@
 type FieldPropsType = { value: 'X' | 'O'; onFieldClick: () => void };
 
 export default function Field({ value, onFieldClick }: FieldPropsType) {
+  const classes = ['field'];
+  if (value) {
+    classes.push('clicked');
+  }
+  if (value === 'O') {
+    classes.push('field-o');
+  }
+
   return (
     <button
-      className="field"
+      className={classes.join(' ')}
       onClick={() => onFieldClick()}
     >
       <p>{value}</p>
